06926d7cf6ba3dc41b1d26cdec1fb3f15587b0f8
Siddharth Sharma <siddharth54330@gmail.com>
2026-08-08 20:40:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from my CI/CD Pipeline! Version 1');
+  res.send('Hello from my CI/CD Pipeline! Version 2 - change2');
 });
 
 app.listen(port, () => console.log(`App running on port ${port}`));
